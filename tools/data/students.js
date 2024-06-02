a1216115976ba0b4cd3e65e47a841787468a5caa
Yusuf Demir <yusufdemir87@gmail.com>
2024-06-02 16:00:11 +0300

    zontroy code generator templates implementation is done.

--- tools/data/students.js
+++ tools/data/students.js
@@ -1,15 +1,15 @@
 export const students = [
   {
     id: 1,
-    name: "Yusuf Demir",
-    slug: "yusuf-demir",
+    name: "John Carter",
+    slug: "john-carter",
     courseId: 1,
     year: 2024
   },
   {
     id: 2,
-    name: "Faruk Temur",
-    slug: "faruk-temur",
+    name: "John Wick",
+    slug: "john-wick",
     courseId: 2,
     year: 2024
   }
